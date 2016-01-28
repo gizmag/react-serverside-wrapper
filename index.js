aae07bf621e4f3c1fea530d58d5070bac0607cac
George <george@gizmag.com>
2016-01-28 15:50:19 +1100

    Converted all code from ES6 to ES5 so that transpiring with Babel is not necessary

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-class ServersideWrapper extends React.Component {
+var ServersideWrapper = React.createClass({
 
-    constructor(props) {
-        super(props);
-    }
+    propTypes: {
+        element: React.PropTypes.string.isRequired,
+        properties: React.PropTypes.object.isRequired
+    },
 
-    render() {
-        const json = JSON.stringify(this.props.properties);
+    render: function() {
+        var json = JSON.stringify(this.props.properties);
 
         return React.createElement('script', { type: 'application/json',
             id: this.props.element + '_props',
             dangerouslySetInnerHTML: { __html: json }
         });
     }
-}
-
-ServersideWrapper.propTypes = {
-    element: React.PropTypes.string.isRequired,
-    properties: React.PropTypes.object.isRequired
-};
+});
 
 // This method is used to create the initial dom node that react attaches itself to
 ServersideWrapper.createDomElement = function(opts) {
     if (typeof window !== 'undefined' && document.getElementById(opts.domElement + '_props') !== null) {
-        const componentProperties = JSON.parse(
+        var componentProperties = JSON.parse(
             document.getElementById(opts.domElement + '_props').innerHTML
         );
         ReactDOM.render(React.createElement(opts.component, componentProperties),
